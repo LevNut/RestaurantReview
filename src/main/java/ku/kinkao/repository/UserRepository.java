d658f7e933d33110063969333263ff006985148f
LevNut <teeranut.s@ku.th>
2022-02-09 12:28:36 +0700

    lab-review assignment

---
+++ src/main/java/ku/kinkao/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package ku.kinkao.repository;
+
+import ku.kinkao.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    // SELECT * FROM User WHERE username = ‘username’
+    User findByUsername(String username);
+
+}
+
