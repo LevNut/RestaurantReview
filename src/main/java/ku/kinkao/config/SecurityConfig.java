b3ccf0ca754a0c3386037004a9cad0de664a5ac4
LevNut <teeranut.s@ku.th>
2022-02-23 11:44:26 +0700

    add login page

--- src/main/java/ku/kinkao/config/SecurityConfig.java
+++ src/main/java/ku/kinkao/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 
 @Configuration
@@ -18,6 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsServiceImp userDetailsService;
+
+    @Autowired
+    private OidcUserService oidcUserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,10 +33,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
 
                 .and()
-                .formLogin()
-                .defaultSuccessUrl("/restaurant", true)
+                        .formLogin()
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/restaurant", true)
+                        .permitAll()
+
                 .and()
-                .logout();
+                        .oauth2Login()
+                        .defaultSuccessUrl("/restaurant", true)
+
+                .and()
+                        .logout()
+                        .logoutUrl("/logout")
+                        .clearAuthentication(true)
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID", "remember-me")
+                        .permitAll();
+
+                ClientRegistrationRepository repository =
+                        getApplicationContext()
+                                .getBean(ClientRegistrationRepository.class);
+
+                if (repository != null) {
+                    http
+                            .oauth2Login().clientRegistrationRepository(repository)
+                            .userInfoEndpoint().oidcUserService(oidcUserService).and()
+                            .loginPage("/login").permitAll();
+                }
+
     }
 
     @Bean
