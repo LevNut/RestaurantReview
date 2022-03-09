b2aecf5c2a063a122929cdb4bfccc4f9551aa425
LevNut <teeranut.s@ku.th>
2022-03-09 11:04:13 +0700

    add DTO, UUID, Input Validation

--- src/main/java/ku/kinkao/repository/RestaurantRepository.java
+++ src/main/java/ku/kinkao/repository/RestaurantRepository.java
@@ -3,8 +3,9 @@ package ku.kinkao.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ku.kinkao.model.Restaurant;
+import java.util.UUID;
 
 @Repository
-public interface RestaurantRepository extends
-        JpaRepository<Restaurant,Integer> {
+public interface RestaurantRepository extends JpaRepository<Restaurant,UUID> {
 }
+
--- src/main/java/ku/kinkao/repository/UserRepository.java
+++ src/main/java/ku/kinkao/repository/UserRepository.java
@@ -3,12 +3,13 @@ package ku.kinkao.repository;
 import ku.kinkao.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User,UUID> {
 
     // SELECT * FROM User WHERE username = ‘username’
     User findByUsername(String username);
-
 }
+
 
